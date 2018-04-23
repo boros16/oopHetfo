4014ae061f8c3bb3c3dc63599a723496839734f2
boros16 <boros16@lab104-13.iit.uni-miskolc.hu>
2018-04-23 16:59:44 +0200

    Hanglemez

---
+++ Hanglemez/src/Hanglemezek.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Hanglemezek {
+	public static void main(String[] args) {
+		int darabszam;
+		int maximalis_hosszu_indexe = 0;
+
+		Scanner beolvasas = new Scanner(System.in);
+		System.out.println("Kerem adja meg a lemezek szamat!");
+		System.out.println("darab=");
+		darabszam = Integer.parseInt(beolvasas.nextLine());
+
+		Hanglemez[] tomb = new Hanglemez[darabszam];
+
+		for (int i = 0; i < darabszam; i++) {
+			tomb[i] = new Hanglemez("E" + (i + 1),
+									"C",
+									(int)(100 * Math.random()) + 1);
+			System.out.println(tomb[i]);
+		}
+
+		for (int i = 1; i < darabszam; i++) {
+			if (tomb[i].hosszabbMint(tomb[maximalis_hosszu_indexe]) == 1) {
+				maximalis_hosszu_indexe = i;
+			}
+		}
+		
+		System.out.println("A leghosszabb lemez adatai:\n"
+							+ tomb[maximalis_hosszu_indexe]);
+
+		System.out.println("Kerem adja meg az eloado nevet!");
+		System.out.println("eloado=");
+		String eloado = beolvasas.nextLine();
+
+		System.out.println(eloado + " lemezei:");
+		
+		for (int i = 0; i < darabszam; i++) {
+			if (tomb[i].eloado(eloado)) {
+				System.out.println(tomb[i]);
+			}
+		}
+		
+		beolvasas.close();
+	}	
+	
+}
